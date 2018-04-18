3e19bd5e54e435812bc78903b48972ff8675f010
ob6d <31044694+ob6d@users.noreply.github.com>
2018-04-18 13:25:15 -0700

    4 - 18 - 18

--- Valeria Quintero - Hangman.py
+++ Valeria Quintero - Hangman.py
@@ -20,21 +20,26 @@ randomWord = random.choice(word_bank)
 
 while guesses_left > 0:
     # Building output
-    guess = input("guess a letter")
-    letters_guessed.append(guess)
-    print(letters_guessed)
-    print()
     output = []
 
     for letter in randomWord:
         if letter in letters_guessed:
             output.append(letter)
-
         else:
             output.append("*")
-
-        if letters_guessed == randomWord:
-            print("You win")
-
     # print(output)
-    print("".join(output))
+    hashed_answer = "".join(output)
+    print(hashed_answer)
+
+    if hashed_answer == randomWord:
+        print("You win!")
+        quit(0)
+
+    guess = input("guess a letter")
+    letters_guessed.append(guess)
+    print(letters_guessed)
+    print()
+    if guess not in randomWord:
+        guesses_left -= 1
+        print("You have %d guesses left" % guesses_left)
+print("You lose.")
