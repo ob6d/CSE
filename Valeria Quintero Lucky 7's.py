8514585de01b493d62b5992c1b2203f73da140cd
ob6d <31044694+ob6d@users.noreply.github.com>
2018-04-23 13:34:40 -0700

    4 - 23 - 18

--- Valeria Quintero Lucky 7's.py
+++ Valeria Quintero Lucky 7's.py
@@ -6,31 +6,33 @@ Extension: If you finish early, what was the maximum amount of money you had thr
 What round did you have the most money?
 """
 import random
+money = 15
+print("You have 15 dollars")
 a = random.randint(1,6)
 b = random.randint(1,6)
 print(a)
 print(b)
 print(a + b)
 total = (a + b)
-money = 15
-maxRoll = round
 round = 0
 
-while money >= 0:
-    a = input
-    b = input
-    total = input
+while money > 0:
+    a = random.randint(1, 6)
+    b = random.randint(1, 6)
+    total = (int(input(a + b)))
     round += 1
 
     if total == 7:
         money += 4
-        print("You have %d dollars left" % money)
+        print("You have %d dollars" % money)
         print("Round %d" % round)
+        break
 
     if total != 7:
         money -= 1
-        print("You have %d dollars left" % money)
+        print("You have %d dollars" % money)
         print("Round %d" % round)
+        break
 
     elif money == 0:
         print("You lost")
